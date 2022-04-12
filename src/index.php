083cd3e98e94c88b0c6ecce9092d478dde003ccd
dhino12 <dhinorahmad0@gmail.com>
2022-04-12 12:03:25 +0700

    add something files

---
+++ src/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/index.css">
+    <link rel="stylesheet" href="./style/background/bg-side.css">
+    <link rel="stylesheet" href="./style/responsive/side-responsive.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <div class="wrapper d-flex" style="width:100%; height: 100vh">
+            <div class="d-flex">
+                <div class="header-brand side-wrapper">
+                    <h1>Hello </h1>
+                    <h1>O</h1>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column header" style="width: 100%">
+                <div class="topbar">
+                <div class="container-fluid h- py-6 py-lg-0 d-flex flex-column flex-sm-row align-items-stretch justify-content-sm-between mt-2">
+                    <div class="page-title d-flex flex-column me-5">
+                        <h1 class="fs-5 mb-0 text-dark my-3">Hello</h1>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#" style="text-decoration: none;">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Library</li>
+                        </ul>
+                    </div>
+                    <div class="d-flex align-items-center overflow-auto me-5">
+                        <form action="" class="mx-3">
+                            <span class="position-absolute ms-2 mt-1">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mh-50px"><path d="M21.7 18.9L18.6 15.8C17.9 16.9 16.9 17.9 15.8 18.6L18.9 21.7C19.3 22.1 19.9 22.1 20.3 21.7L21.7 20.3C22.1 19.9 22.1 19.3 21.7 18.9Z" fill="black"></path><path opacity="0.3" d="M11 20C6 20 2 16 2 11C2 6 6 2 11 2C16 2 20 6 20 11C20 16 16 20 11 20ZM11 4C7.1 4 4 7.1 4 11C4 14.9 7.1 18 11 18C14.9 18 18 14.9 18 11C18 7.1 14.9 4 11 4ZM8 11C8 9.3 9.3 8 11 8C11.6 8 12 7.6 12 7C12 6.4 11.6 6 11 6C8.2 6 6 8.2 6 11C6 11.6 6.4 12 7 12C7.6 12 8 11.6 8 11Z" fill="black"></path></svg>
+                            </span>
+                            <input type="email" class="form-control ps-5" style="border-radius: 8px;" id="exampleFormControlInput1" placeholder="search">
+                        </form>
+                        
+                        <div class="d-flex align-center btn btn-outline-light rounded-6-cs me-2">
+                            <a href="">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mh-50px"><rect x="2" y="2" width="9" height="9" rx="2" fill="black"></rect><rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="black"></rect><rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="black"></rect><rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="black"></rect></svg>
+                            </a>
+                        </div>
+                        <div class="d-flex align-center btn btn-outline-light rounded-6-cs me-2" id="button">
+                            <a href="">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mh-50px"><path opacity="0.3" d="M20 3H4C2.89543 3 2 3.89543 2 5V16C2 17.1046 2.89543 18 4 18H4.5C5.05228 18 5.5 18.4477 5.5 19V21.5052C5.5 22.1441 6.21212 22.5253 6.74376 22.1708L11.4885 19.0077C12.4741 18.3506 13.6321 18 14.8167 18H20C21.1046 18 22 17.1046 22 16V5C22 3.89543 21.1046 3 20 3Z" fill="black"></path><rect x="6" y="12" width="7" height="2" rx="1" fill="black"></rect><rect x="6" y="7" width="12" height="2" rx="1" fill="black"></rect></svg>
+                            </a>
+                        </div>
+                        <div class="d-flex align-center btn btn-outline-light rounded-6-cs me-2 bg-info">
+                            <a href="" class="text-decoration-none light fw-bold" style="color: white;">
+                                2
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
