83c599b461dca706b497f20af7ade6d2309a8b13
dhino12 <dhinorahmad0@gmail.com>
2022-04-19 11:29:58 +0700

    add return array numerik

--- src/script/functions.php
+++ src/script/functions.php
@@ -2,21 +2,29 @@
 
 include 'conn.php';
 
-function query(String $query)
+function query(String $query, bool $assoc)
 {
     global $conn;
     $result = mysqli_query($conn, $query);
-
+    
     if (!$result) {
         return mysqli_error($conn);;
     }
 
-    $datas = [];
-    while ($data = mysqli_fetch_assoc($result)) {
-        $datas[] = $data;
+    if ($assoc) {
+        $datas = [];
+        while ($data = mysqli_fetch_assoc($result)) {
+            $datas[] = $data;
+        }
+    }else {
+        $datas = [];
+        while ($data = mysqli_fetch_row($result)) {
+            foreach($data as $key => $value) {
+                $datas[] = $value;
+
+            }
+        }
     }
 
     return $datas;
 }
-
-?>
