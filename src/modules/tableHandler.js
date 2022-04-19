3a536f6b28052060306bf797cf293a18139c3435
dhino12 <dhinorahmad0@gmail.com>
2022-04-19 14:47:48 +0700

    add table dynamic

---
+++ src/modules/tableHandler.js
@@ -0,0 +1,69 @@
+const btnCreateTable = document.querySelector('#btnCreateTable');
+const formInput = document.querySelector('#form-input');
+
+function crTable(idForm) {
+    const createTable = document.createElement('table');
+    const createThead = document.createElement('thead');
+    const createTr = document.createElement('tr'); 
+    const createTr2 = document.createElement('tr'); 
+    const createTr3 = document.createElement('tr'); 
+
+    const createTh1 = document.createElement('th');
+    const createTh2 = document.createElement('th');
+
+    const createTd1 = document.createElement('td');
+    const createTd2 = document.createElement('td');
+
+    const createInput1 = document.createElement('input');
+    const createInput2 = document.createElement('input');
+    const createInput3 = document.createElement('input');
+    const createInput4 = document.createElement('input');
+    
+    createTable.className = 'table table-bordered table-striped table-hover';
+    createInput1.className = 'form-control border-0';
+    createInput1.placeholder = 'Masukan Data';
+    createInput1.type = 'text';
+     
+    createInput2.className = 'form-control border-0';
+    createInput2.placeholder = 'Masukan Data';
+    createInput2.type = 'text';
+
+    createInput3.className = 'form-control border-0';
+    createInput3.placeholder = 'Masukan Data';
+    createInput3.type = 'text';
+
+    createInput4.className = 'form-control border-0';
+    createInput4.placeholder = 'Masukan Data';
+    createInput4.type = 'text';
+
+    // head table
+    createTh1.appendChild(createInput1);
+    createTh2.appendChild(createInput2);
+
+    createTr.appendChild(createTh1); 
+    createTr.appendChild(createTh2); 
+
+    createThead.appendChild(createTr);
+    createTable.appendChild(createThead);
+    formInput.appendChild(createTable);
+    
+    // ==========================================
+    createTd1.appendChild(createInput3)
+    createTd2.appendChild(createInput4)
+    createTr2.appendChild(createTd1)
+    createTr2.appendChild(createTd2)
+    createTable.appendChild(createTr2) 
+    formInput.appendChild(createTable)
+}
+
+btnCreateTable.addEventListener('click', () => {
+
+    // createTd.appendChild(createInput);
+    // createTr.appendChild(createTd);
+    // createTr.appendChild(createTd);
+
+    // createTable.appendChild(createTr);
+    // createTable.appendChild(createTr);
+
+    crTable();
+});
