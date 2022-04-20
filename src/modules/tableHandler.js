36b7eeb7d63f1a66dced186a1c343f04566951c9
dhino12 <dhinorahmad0@gmail.com>
2022-04-20 13:54:32 +0700

    add solve to name based on column/row

--- src/modules/tableHandler.js
+++ src/modules/tableHandler.js
@@ -1,16 +1,71 @@
-import { crTable } from './table/createTable'
+// import { crTable } from "./table/createTable";
 
-const btnCreateTable = document.querySelector('#btnCreateTable');
-const formInput = document.querySelector('#form-input');
+const btnCreateTable = document.querySelector("#btnCreateTable");
+const btnCreateRows = document.querySelector("#addRow");
+const btnCreateColumn = document.querySelector("#addColumn");
 
-btnCreateTable.addEventListener('click', () => {
+const btnSubmitCrTable = document.querySelector('#submit');
 
-    // createTd.appendChild(createInput);
-    // createTr.appendChild(createTd);
-    // createTr.appendChild(createTd);
+const formInput = document.querySelector("#form-input");
 
-    // createTable.appendChild(createTr);
-    // createTable.appendChild(createTr);
+const titleTable = document.querySelector('#titleTable');
+const columnTotal = document.querySelector('#columnTotal');
+const rowTotal = document.querySelector('#rowsTotal');
 
-    crTable(formInput);
-});
+function crTable(){
+    const valColumnTotal = columnTotal.value;
+    const valRowTotal = rowTotal.value;
+    const valTitle = titleTable.value;
+    console.log(valColumnTotal);
+    console.log(valRowTotal);
+
+    const createTable = document.createElement('table');
+    createTable.className = 'table table-bordered table-striped table-hover';
+    const createThead = document.createElement('thead');
+    const createTbody = document.createElement('tbody');
+    const crInputTitle = document.createElement('input');        
+
+    for (let row = 1; row <= valRowTotal; row++) {
+        const createTr = document.createElement('tr'); 
+        console.log(`row ======= : ${row}`);
+        
+        for (let col = 1; col <= valColumnTotal; col++) {
+            console.log(`col : ${col}`);
+        
+            const createTh1 = document.createElement('th');
+                
+            const createInput1 = document.createElement('input');        
+            createInput1.className = 'form-control border-0';
+            createInput1.placeholder = 'Masukan Data';
+            createInput1.type = 'text';
+            createInput1.name = `${row}${col}`;
+ 
+            createTh1.appendChild(createInput1);
+            createTr.appendChild(createTh1);
+        }
+        if (row === 1) createThead.appendChild(createTr);
+        else createTbody.appendChild(createTr);
+    }
+    crInputTitle.className = 'form-control border-0 my-2';
+    crInputTitle.placeholder = 'Masukan Data';
+    crInputTitle.type = 'text';
+    crInputTitle.name = `titleTable`;
+    crInputTitle.value = valTitle
+ 
+    createTable.appendChild(createThead);
+    createTable.appendChild(createTbody);
+    formInput.appendChild(crInputTitle);
+    formInput.appendChild(createTable);
+}
+
+// btnCreateTable.addEventListener("click", () => {
+    //     crTable(formInput);
+// });
+    
+btnSubmitCrTable.addEventListener('click', () => {
+    btnCreateColumn.removeAttribute("disabled");
+    btnCreateRows.removeAttribute("disabled");
+
+    console.log('ter click modal');
+    crTable();
+})
