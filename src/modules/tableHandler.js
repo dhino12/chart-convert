7f76a85382778106a98434e50ac4b18a00c8de0e
dhino12 <dhinorahmad0@gmail.com>
2022-04-21 13:09:33 +0700

    add simantic code

--- src/modules/tableHandler.js
+++ src/modules/tableHandler.js
@@ -12,6 +12,36 @@ const titleTable = document.querySelector('#titleTable');
 const columnTotal = document.querySelector('#columnTotal');
 const rowTotal = document.querySelector('#rowsTotal');
 
+function crElement(titleTable) {
+    const createTable = document.createElement('table');
+    const createThead = document.createElement('thead');
+    const createTbody = document.createElement('tbody');
+    const crInputTitle = document.createElement('input');
+    const crButtonSubmit = document.createElement('button');
+    const createTr = document.createElement('tr'); 
+    const createTd = document.createElement('td');
+    const createTh1 = document.createElement('th');
+    const createInput1 = document.createElement('input');
+
+    createTable.className = 'table table-bordered table-striped table-hover';
+    crButtonSubmit.type = 'submit'
+    crButtonSubmit.name = 'submit'
+    crButtonSubmit.innerText = 'Submit'
+    crButtonSubmit.className = 'btn btn-outline-primary';
+
+    crInputTitle.className = 'form-control border-0 my-2';
+    crInputTitle.placeholder = 'Masukan Judul Table';
+    crInputTitle.type = 'text';
+    crInputTitle.name = `titleTable`;
+    crInputTitle.value = titleTable
+    crInputTitle.required
+
+    return {
+        createTable, createThead, createTbody, crInputTitle, 
+        crButtonSubmit, createTr, createTd, createTh1, createInput1
+    }
+}
+
 function crTable(){
     const valColumnTotal = columnTotal.value;
     const valRowTotal = rowTotal.value;
@@ -19,20 +49,15 @@ function crTable(){
     console.log(valColumnTotal);
     console.log(valRowTotal);
 
-    const createTable = document.createElement('table');
-    createTable.className = 'table table-bordered table-striped table-hover';
-    const createThead = document.createElement('thead');
-    const createTbody = document.createElement('tbody');
-    const crInputTitle = document.createElement('input');
-    const crButtonSubmit = document.createElement('button');
-    crButtonSubmit.type = 'submit'
-    crButtonSubmit.name = 'submit'
-    crButtonSubmit.innerText = 'Submit'
-    crButtonSubmit.className = 'btn btn-outline-primary';
-
+    const { createTable, 
+        createThead, 
+        createTbody, 
+        crInputTitle, 
+        crButtonSubmit 
+    } = crElement(valTitle);
+    
     for (let row = 1; row <= valRowTotal; row++) {
-        const createTr = document.createElement('tr'); 
-        const createTd = document.createElement('td');
+        const { createTr, createTd } = crElement(valTitle); 
         createTd.innerText = row;
         createTr.appendChild(createTd);
         console.log(`row ======= : ${row}`);
@@ -40,13 +65,13 @@ function crTable(){
         for (let col = 1; col <= valColumnTotal; col++) {
             console.log(`col : ${col}`);
         
-            const createTh1 = document.createElement('th');
-                
-            const createInput1 = document.createElement('input');        
+            const {createTh1, createInput1} = crElement(valTitle);
+
             createInput1.className = 'form-control border-0';
             createInput1.placeholder = 'Masukan Data';
             createInput1.type = 'text';
-            createInput1.name = `${row}${col}`;
+            createInput1.name = `${row}-${col}`;
+            createInput1.autocomplete = 'off'
  
             createTh1.appendChild(createInput1);
             createTr.appendChild(createTh1);
@@ -54,12 +79,6 @@ function crTable(){
         if (row === 1) createThead.appendChild(createTr);
         else createTbody.appendChild(createTr);
     }
-    crInputTitle.className = 'form-control border-0 my-2';
-    crInputTitle.placeholder = 'Masukan Judul Table';
-    crInputTitle.type = 'text';
-    crInputTitle.name = `titleTable`;
-    crInputTitle.value = valTitle
-    crInputTitle.required
  
     createTable.appendChild(createThead);
     createTable.appendChild(createTbody);
@@ -68,11 +87,8 @@ function crTable(){
     formInput.appendChild(crButtonSubmit);
 }
 
-// btnCreateTable.addEventListener("click", () => {
-    //     crTable(formInput);
-// });
-    
-btnSubmitCrTable.addEventListener('click', () => {
+btnSubmitCrTable.addEventListener('click', (e) => {
+    e.preventDefault();
     btnCreateColumn.removeAttribute("disabled");
     btnCreateRows.removeAttribute("disabled");
 
