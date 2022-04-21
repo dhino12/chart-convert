ae93accd9a0961d0986cdcea65d921af8c0c3322
dhino12 <dhinorahmad0@gmail.com>
2022-04-21 13:10:12 +0700

    addValue & crTable (concat string query)

--- src/script/functions.php
+++ src/script/functions.php
@@ -1,10 +1,23 @@
 <?php
 
-use function PHPSTORM_META\type;
-
 include 'conn.php';
 
-function query(String $query, bool $assoc)
+function conncatQuery(array $tData, string $flag)
+{
+    $strTCon = "";
+    foreach ($tData as $key => $value) {
+        if ((count($tData) - 1) == $key) {
+            $strTCon .= "`$value` $flag";
+
+        } else {
+            $strTCon .= "`$value` $flag,";
+        }
+    }
+
+    return $strTCon;
+}
+
+function query(String $query, $assoc)
 {
     global $conn;
     $result = mysqli_query($conn, $query);
@@ -13,19 +26,24 @@ function query(String $query, bool $assoc)
         return mysqli_error($conn);;
     }
 
-    if ($assoc) {
+    if ($assoc === true) {
+        // kembalikan data array assosiative
         $datas = [];
         while ($data = mysqli_fetch_assoc($result)) {
             $datas[] = $data;
         }
-    }else {
+    }else if ($assoc === false) {
+        // kembalikan data array numerik
         $datas = [];
         while ($data = mysqli_fetch_row($result)) {
             foreach($data as $key => $value) {
                 $datas[] = $value;
-
+                
             }
         }
+    } else {
+        // jika input data
+        return $result;
     }
 
     return $datas;
@@ -33,25 +51,37 @@ function query(String $query, bool $assoc)
 
 function crTableDb($datas)
 {
-    global $conn;
-
     $tTitle = $datas['titleTable'];
     $tHead = [];
     $tBody = []; 
-    foreach ($datas as $key => $value) { 
+    foreach ($datas as $key => $value) {
         if (explode('-', $key)[0] === '1') {
             $tHead[] = $value;
-        } else {
-            $tBody = $value; 
+        } else if(is_numeric(explode('-', $key)[0])) {
+            $tBody[] = $value; 
         }
     }
     
-    $strThead = "";
-    foreach ($tHead as $key => $value) {
-        $strThead .= "$value VARCHAR(180), ";
-    }
-    var_dump($strThead);
-    $result = query("CREATE TABLE `$tTitle` ($strThead)", true);
+    $strThead = conncatQuery($tHead, 'VARCHAR(180)');
+    
+    $result = query("CREATE TABLE `$tTitle` ($strThead);", '');
 
-    return $result;
+    if (is_string($result)) {
+        return $result; // kembalikan pesan kesalahan
+    }
+    
+    return $tBody;
+}
+
+function addValue($tBodyDatas, $tTitle)
+{
+    
+    $strTBody = conncatQuery($tBodyDatas, '');
+    $fixStrTBody = str_replace('`', "'", $strTBody);
+    var_dump($fixStrTBody);
+    $query = "INSERT INTO `$tTitle` VALUES ($fixStrTBody);";
+    var_dump($query);
+    $data = query($query, '');
+
+    return $data;
 }
