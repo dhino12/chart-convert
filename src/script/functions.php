805727a4c95f2c43ab7a55c09c1d96a821f36d69
dhino12 <dhinorahmad0@gmail.com>
2022-04-22 23:04:12 +0700

    solution multiple query

--- src/script/functions.php
+++ src/script/functions.php
@@ -2,19 +2,22 @@
 
 include 'conn.php';
 
-function conncatQuery(array $tData, string $flag)
+function splitArray(array $datas)
 {
-    $strTCon = "";
-    foreach ($tData as $key => $value) {
-        if ((count($tData) - 1) == $key) {
-            $strTCon .= "`$value` $flag";
-
-        } else {
-            $strTCon .= "`$value` $flag,";
+    $tHead = [];
+    $tBody = [];
+    $tCollaction = [];
+    foreach ($datas as $key => $value) {
+        if (explode('-', $key)[0] === '1') {
+            $tHead[] = $value;
+        } else if(is_numeric(explode('-', $key)[0])) {
+            $tBody[] = $value; 
         }
-    }
+    } 
 
-    return $strTCon;
+    array_push($tCollaction, $tHead, $tBody); 
+
+    return $tCollaction;
 }
 
 function query(String $query, $assoc)
@@ -52,17 +55,20 @@ function query(String $query, $assoc)
 function crTableDb($datas)
 {
     $tTitle = $datas['titleTable'];
-    $tHead = [];
-    $tBody = []; 
-    foreach ($datas as $key => $value) {
-        if (explode('-', $key)[0] === '1') {
-            $tHead[] = $value;
-        } else if(is_numeric(explode('-', $key)[0])) {
-            $tBody[] = $value; 
+    $tCollaction = splitArray($datas);
+    $tHead[] = $tCollaction[0]; 
+    
+    $strThead = '';
+
+    foreach($tHead[0] as $key => $value) {
+        if(count($tHead) == $key) {
+            $strThead .= "`$value` VARCHAR(180)";
+
+        } else {
+            $strThead .= "`$value` VARCHAR(180),";
+
         }
     }
-    
-    $strThead = conncatQuery($tHead, 'VARCHAR(180)');
     
     $result = query("CREATE TABLE `$tTitle` ($strThead);", '');
 
@@ -70,18 +76,38 @@ function crTableDb($datas)
         return $result; // kembalikan pesan kesalahan
     }
     
-    return $tBody;
+    return $tCollaction;
 }
 
-function addValue($tBodyDatas, $tTitle)
+function addValue($tBodyDatas, $tTitle, $tHead)
 {
     
-    $strTBody = conncatQuery($tBodyDatas, '');
-    $fixStrTBody = str_replace('`', "'", $strTBody);
-    var_dump($fixStrTBody);
-    $query = "INSERT INTO `$tTitle` VALUES ($fixStrTBody);";
-    var_dump($query);
+    $strTBody = '';
+    var_dump(count($tBodyDatas));
+
+    for ($i = 0; $i <= count($tBodyDatas) - 1 ; $i++) {
+        if($i === 0) {
+            echo 'masuk if <br>';
+            $strTBody .= "('". $tBodyDatas[$i] ."',";
+            
+        } else if ((count($tBodyDatas) - 1) === $i) {
+            echo 'masuk else if 1 <br>';
+            $strTBody .= "'". $tBodyDatas[$i] ."')";
+            
+        } else if ($i % count($tHead) == 0) {
+            echo 'masuk else if 2 <br>';
+            $strTBody .= "),(";
+            
+        } else {
+            echo 'masuk else <br>';
+            $strTBody .= "'". $tBodyDatas[$i] ."',";
+
+        }
+    }
+
+    $query = "INSERT INTO `$tTitle` VALUES $strTBody;";
     $data = query($query, '');
+    var_dump($data);
 
     return $data;
 }
