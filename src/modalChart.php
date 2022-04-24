4bb9ceb89d3e4822d6bf2a256ded3dd44b20b7f9
dhino12 <dhinorahmad0@gmail.com>
2022-04-24 20:26:45 +0700

    encytype= and fix excel to db

--- src/modalChart.php
+++ src/modalChart.php
@@ -5,10 +5,14 @@
                 <h5 class="modal-title" id="modalChartLabel">Import Excel</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form>
+            <form action="import.php" method="POST" enctype="multipart/form-data">
                 <div class="modal-body">
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Pilih Tipe Grafik</label>
+                        <label for="title" class="form-label">Judul Table <b style="color: red">*</b> </label>
+                        <input type="text" class="form-control" id="title" name="title_table" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Pilih Tipe Grafik <b style="color: red">*</b></label>
                         <select name="type" id="" class="form-control">
                             <option value="line">line</option>
                             <option value="pie">pie</option>
@@ -17,12 +21,12 @@
                     </div>
                     <div class="mb-3">
                         <label for="UploadFiles" class="form-label">Upload Files</label>
-                        <input type="file" class="form-control" id="UploadFiles" accept="xlsx">
+                        <input type="file" class="form-control" id="UploadFiles" name="excel_file" accept="xlsx">
                     </div> 
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" name="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                 </div>
             </form>
         </div>
