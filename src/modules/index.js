448c2c152e5c443e35997c6a755836d061f7c897
dhino12 <dhinorahmad0@gmail.com>
2022-04-25 10:28:43 +0700

    remove space

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,11 @@
 const menuLinks = document.querySelectorAll(".menu-link");
 const containerAside = document.querySelector(".aside-menu");
-const canvasGrafik = document.querySelector('#canvas-grafik') 
+const canvasGrafik = document.querySelector('#canvas-grafik')
 const btnStratchs = document.querySelectorAll('#btn-stratch');
 const wrapCanvas = document.querySelectorAll('#wrapper-canvas');
 
 containerAside.addEventListener("click", (e) => {
-    if (e.target.className.includes('menu-link')) { 
+    if (e.target.className.includes('menu-link')) {
         menuLinks.forEach((menuLink) => {
             menuLink.className = "menu-link";
         });
@@ -13,17 +13,16 @@ containerAside.addEventListener("click", (e) => {
         e.target.classList.add("active-menu");
     }
 });
- 
-function toggleStracth (btnStratchs, i) {
+
+function toggleStracth(btnStratchs, i) {
     if (btnStratchs.target.className.includes('bg-light')) {
         btnStratchs.target.classList.remove('bg-light');
         wrapCanvas[i].classList.replace('col-xl-12', 'col-xl-6');
         console.log('masuk if');
-        
+
     } else {
         btnStratchs.target.classList.add('bg-light');
         wrapCanvas[i].classList.replace('col-xl-6', 'col-xl-12');
-
     }
 }
 
