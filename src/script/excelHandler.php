54f3db2bafebc55d64a0818109bf806573560a5c
dhino12 <dhinorahmad0@gmail.com>
2022-05-03 13:14:55 +0700

    progress di hari lebaran

--- src/script/excelHandler.php
+++ src/script/excelHandler.php
@@ -4,8 +4,8 @@ require '../vendor/autoload.php';
 
 function getExcelFile($dataFiles)
 {
-    $arr_file = explode('.', $dataFiles['excel_file']['name']);
-    $extension = end($arr_file);
+    $arr_file = explode('.', $dataFiles['excel_file']['name']); // pecah kedalam array diantara . 
+    $extension = end($arr_file); // mndapatkan extension .xlsx / .xls`
  
     if('csv' == $extension) {
         $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
@@ -18,8 +18,9 @@ function getExcelFile($dataFiles)
     return $spreadsheet;
 }
 
-function fixArray(array $datas)
+function fixArray(array $datas): array
 {
+    // remove NULL data
     $fixArr = [];
 
     foreach($datas as $data) {
@@ -33,22 +34,80 @@ function fixArray(array $datas)
         }
         $fixArr[] = $tmpArr;
     }
-
+    
     return $fixArr;
 }
 
 function getDataCurrentSheet($sheetNames, $spreadsheet)
-{
-    $dataAllSheet = [];
+{ 
     $dataSheet = [];
-    $titleTable = [];
     foreach($sheetNames as $name) {
         if($name === NULL) continue;
-        $tmpData = fixArray($spreadsheet->getSheetByName($name)->toArray());
-        $dataSheet = $tmpData;
-        
+        $dataSheet[] = fixArray($spreadsheet->getSheetByName($name)->toArray());
     }
-    var_dump($dataSheet);
+    
+    return $dataSheet;
 }
 
+function  crTableSheet(array $sheetDatas)
+{
+    $tmpTitle = [];
+    foreach($sheetDatas as $currSheetDatas) {
+        foreach($currSheetDatas as $key => $data) {
+            if (count($data) === 1 ) {
+                $tmpTitle[] = ($data[0] === NULL)? "untitled" : $data[0] ;
+            }
+
+        }
+    }
+    // var_dump($tmpTitle);
+}
+
+/*
+array(7) {
+    [0]=>
+    array(73) {
+      [0]=>
+      array(1) {
+        [0]=>
+        string(33) "Laju Pertumbuhan Ekonomi Nasional"
+      }
+      [1]=>
+      array(2) {
+        [0]=>
+        string(16) "Ekonomi Nasional"
+        [1]=>
+        string(23) "Pertumbuhan Ekonomi (%)"
+      }
+      [2]=>
+      array(2) {
+        [0]=>
+        string(9) "2018 (Q1)"
+        [1]=>
+        float(5.07000000000000028421709430404007434844970703125)
+      }
+      [3]=>
+      array(2) {
+        [0]=>
+        string(9) "2018 (Q2)"
+        [1]=>
+        float(5.269999999999999573674358543939888477325439453125)
+      }
+      [4]=>
+      array(2) {
+        [0]=>
+        string(9) "2018 (Q3)"
+        [1]=>
+        float(5.1699999999999999289457264239899814128875732421875)
+      }
+      [5]=>
+      array(2) {
+        [0]=>
+        string(9) "2018 (Q4)"
+        [1]=>
+        float(5.17999999999999971578290569595992565155029296875)
+      }
+    ]
+]
+*/
 ?>
