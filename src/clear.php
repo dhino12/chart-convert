f996b54f820c57e0e03f09a7c2ce7cd1785c52d2
dhino12 <dhinorahmad0@gmail.com>
2022-06-09 16:00:11 +0700

    clear data

--- src/clear.php
+++ src/clear.php
@@ -4,7 +4,7 @@ include 'script/functions.php';
 session_start();
 
 if (isset($_SESSION['identity'])) {
-    $result = clearData($_SESSION['identity']);
+    $result = clearData($_SESSION['identity'], $_SESSION['level']);
     if ($result >= 0) {
         header("Location: index.php");
     }
