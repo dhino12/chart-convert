4ac3e86e959126aea5ce8d5bf52fe89dc8f3aa67
dhino12 <dhinorahmad0@gmail.com>
2022-06-09 15:59:33 +0700

    add user

---
+++ src/addUser.php
@@ -0,0 +1,32 @@
+<?php 
+
+include 'script/functions.php';
+
+session_start();
+
+if (isset($_POST['submit'])) {
+    $id = $_SESSION['identity'];
+    $name = $_POST['fullName'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "INSERT INTO users (
+            foto, username, name, password,
+            level, email, id
+        ) VALUES (
+            'person.svg', '$username',
+            '$name', '$password',
+            'user', '$email', '$id'
+        );";
+    $msgHandler = query($query, '');
+    
+    if ($msgHandler === true) {
+        header("Location: userManagement.php");
+        exit;
+    } else {
+        echo "<script>alert('gagal menambahkan user')</script>";
+    }
+}
+
+?>
