a180c57bbfd8d4415ed31f1f723373dd85054169
dhino12 <dhinorahmad0@gmail.com>
2022-06-09 15:59:54 +0700

    delete user

--- src/delete.php
+++ src/delete.php
@@ -6,14 +6,15 @@ include 'script/functions.php';
 if (isset($_GET['tableName'])) {
     $tableName = $_GET['tableName'];
     $id = $_SESSION['identity'];
+    $level = $_SESSION['level'];
     $strTables = '';
 
-    $tableNames = query("SELECT table_name FROM `users` WHERE id='$id'", true)[0]['table_name'];
+    $tableNames = query("SELECT table_name FROM `$level` WHERE id='$id'", true)[0]['table_name'];
 
     $tableNames = deleteData("DROP TABLE `$tableName`", $tableNames, $tableName);
     
     if (is_string($tableNames)) {
-        $result = query("UPDATE `users` SET `table_name`='$tableNames' WHERE `id`='$id'", '');
+        $result = query("UPDATE `$level` SET `table_name`='$tableNames' WHERE `id`='$id'", '');
         echo "<script>alert('Data $tableName berhasil dihapus')</script>";
 
         header("Location: index.php");
