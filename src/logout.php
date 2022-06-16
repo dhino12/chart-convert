11c0fcce661e678f12b1cc0a48094517a6dbdb95
dhino12 <dhinorahmad0@gmail.com>
2022-06-16 14:56:22 +0700

    add status logout

--- src/logout.php
+++ src/logout.php
@@ -1,7 +1,13 @@
 <?php 
 session_start();
+
+include 'src/script/functions.php';
+$username = $_SESSION['username'];
+$level = $_SESSION['level'];
+$queryUpdateStatus = "UPDATE $level SET status='unactive' WHERE username='$username'";
+query($queryUpdateStatus, '');
+
 session_destroy();
 $_SESSION = [];
-
 header("Location: ../login.php");
 ?>
