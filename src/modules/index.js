a304d58510bb20b7fd6dd18e3a234b83395f4001
dhino12 <dhinorahmad0@gmail.com>
2022-06-20 16:13:50 +0700

    fix index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,51 +1,33 @@
 const menuLinks = document.querySelectorAll(".menu-link");
 const containerAside = document.querySelector(".aside-menu");
-const canvasGrafik = document.querySelector('#canvas-grafik')
-const btnStratchs = document.querySelectorAll('#btn-stratch');
-const wrapCanvas = document.querySelectorAll('#wrapper-canvas');
-const buttonStract = document.querySelector("#btn-stracting");
-let indicator = true;
+const canvasGrafik = document.querySelector("#canvas-grafik");
+const btnStratchs = document.querySelectorAll("#btn-stratch");
+const wrapCanvas = document.querySelectorAll("#wrapper-canvas");
 
 containerAside.addEventListener("click", (e) => {
-    if (e.target.className.includes('menu-link')) {
-        menuLinks.forEach((menuLink) => {
-            menuLink.className = "menu-link";
-        });
+  if (e.target.className.includes("menu-link")) {
+    menuLinks.forEach((menuLink) => {
+      menuLink.className = "menu-link";
+    });
 
-        e.target.classList.add("active-menu");
-    }
+    e.target.classList.add("active-menu");
+  }
 });
 
 function toggleStracth(btnStratchs, i) {
-    if (btnStratchs.target.className.includes('bg-light')) {
-        btnStratchs.target.classList.remove('bg-light');
-        wrapCanvas[i].classList.replace('col-xl-12', 'col-xl-6');
-        console.log('masuk if');
-
-    } else {
-        btnStratchs.target.classList.add('bg-light');
-        wrapCanvas[i].classList.replace('col-xl-6', 'col-xl-12');
-    }
+  if (btnStratchs.target.className.includes("bg-light")) {
+    btnStratchs.target.classList.remove("bg-light");
+    wrapCanvas[i].classList.replace("col-xl-12", "col-xl-6");
+    console.log("masuk if");
+  } else {
+    btnStratchs.target.classList.add("bg-light");
+    wrapCanvas[i].classList.replace("col-xl-6", "col-xl-12");
+  }
 }
 
 btnStratchs.forEach((e, i) => {
-    e.addEventListener('click', (e) => {
-        e.preventDefault();
-        toggleStracth(e, i);
-    })
-})
-
-buttonStract.addEventListener('click', () => {
-    if (indicator) {
-        wrapCanvas.forEach(e => {
-            e.classList.replace('col-xl-6', 'col-xl-12');
-        });
-        indicator = false
-    }else {
-        wrapCanvas.forEach(e => {
-            e.classList.replace('col-xl-12', 'col-xl-6');
-        });
-        indicator = true
-    }
-})
-
+  e.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleStracth(e, i);
+  });
+});
