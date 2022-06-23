9ced95470c5b1c1619bc24cb5a3f478480b35fda
dhino12 <dhinorahmad0@gmail.com>
2022-06-23 15:05:41 +0700

    update user with unactive

--- src/logout.php
+++ src/logout.php
@@ -2,14 +2,11 @@
 session_start();
 
 include 'script/functions.php';
-$username = $_SESSION['identity'];
+$username = $_SESSION['username'];
 $level = $_SESSION['level'];
-var_dump($username);
 
 $queryUpdateStatus = "UPDATE $level SET status='unactive' WHERE username='$username'";
 $result = query($queryUpdateStatus, '');
-var_dump($result);
-die;
 
 session_destroy();
 $_SESSION = [];
